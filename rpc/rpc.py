3d302a334b8e9d11debed43a7837f7e1fac186b0
Aspect13 <ad13box@gmail.com>
2023-01-04 15:37:32 +0400

    bug fixes

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -26,19 +26,19 @@ class RPC:
 
     @web.rpc('tasks_update_env')
     @rpc_tools.wrap_exceptions(RuntimeError)
-    def update_env(self, project_id: int, task_id: int, env_vars: dict, rewrite: bool = True) -> bool:
+    def update_env(self, project_id: int, task_id: int, env_vars: str, rewrite: bool = True) -> bool:
         project = self.context.rpc_manager.call.project_get_or_404(project_id)
         if rewrite:
-            Task.query.filter(Task.id == task_id).update({Task.env_vars: json.dumps(env_vars, ensure_ascii=False)})
+            Task.query.filter(Task.task_id == task_id).update({Task.env_vars: env_vars})
         else:
-            task_vars = Task.query.with_entities(Task.env_vars).filter(Task.id == task_id).first()
+            task_vars = Task.query.with_entities(Task.env_vars).filter(Task.task_id == task_id).first()
             try:
                 task_vars = task_vars[0]
             except IndexError:
                 log.error('Cannot find task with id: %s', task_id)
                 return False
             task_vars = json.loads(task_vars)
-            task_vars.update(**env_vars)
-            Task.query.filter(Task.id == task_id).update({Task.env_vars: json.dumps(task_vars, ensure_ascii=False)})
+            task_vars.update(**json.loads(env_vars))
+            Task.query.filter(Task.task_id == task_id).update({Task.env_vars: task_vars})
         Task.commit()
         return True
