3c6ca2a4ea88b6096ed1ba4697ea43f55c4449b7
Aspect13 <ad13box@gmail.com>
2023-07-19 15:56:42 +0400

    fixing some tasks apis fixing various typos fixing classes inheritance

--- api/v1/run_task.py
+++ api/v1/run_task.py
@@ -1,69 +1,72 @@
 from flask import request
+from sqlalchemy import or_
 
-# from ...constants import TASK_STATUS
 from ...models.tasks import Task
-# from ...models.results import TaskResults
-
-from tools import VaultClient
-from pylon.core.tools import log
-
 from ...tools.TaskManager import TaskManager
-from tools import api_tools, auth
+
+from tools import api_tools, auth, VaultClient, config as c
 
 
 class ProjectApi(api_tools.APIModeHandler):
-    def _get_task(self, project_id: int, task_id: str):
-        return self.module.context.rpc_manager.call.project_get_or_404(project_id=project_id), \
-               Task.query.filter(Task.task_id == task_id, Task.mode == self.mode).first()
+    @staticmethod
+    def _get_task(project_id: int, task_id: str):
+        return Task.query.filter(
+            Task.task_id == task_id,
+            or_(
+                Task.project_id == project_id,
+                Task.mode == c.ADMINISTRATION_MODE
+            )
+        ).first_or_404()
 
     @auth.decorators.check_api(["configuration.tasks.tasks.view"])
     def get(self, project_id: int, task_id: str):
-        # args = self.get_parser.parse_args(strict=False)
-        args = request.args
-        project, task = self._get_task(project_id, task_id)  # todo: why do we extra query project?
-
-        if args.get("exec"):
-            vault_client = VaultClient.from_project(project)
+        task = self._get_task(project_id, task_id)
+        if request.args.get('exec'):
+            vault_client = VaultClient(project_id)
             return vault_client.unsecret(task.to_json())
         return task.to_json(), 200
 
     @auth.decorators.check_api(["configuration.tasks.tasks.create"])
     def post(self, project_id: int, task_id: str):
-        project, task = self._get_task(project_id, task_id)  # todo: why do we extra query project?
+        task = self._get_task(project_id, task_id)
         try:
             event = [{row['name']: row['default'] for row in request.json}]
         except:
-            event = request.json if isinstance(request.json, list) else [request.json]
-        # resp = TaskManager(project.id).run_task(event, task.task_id)
-        resp = TaskManager(project_id=project.id, mode=self.mode).run_task(event, task.task_id)
-        # todo: why do you think task_result_id will be correct?
-        # task_result_id = TaskResults.query.filter_by(task_id=task_id, project_id=project_id).order_by(
-        #     TaskResults.id.desc()).first()
-        # if resp['code'] == 200 and task_result_id:
-        #     task_result_id.task_status = TASK_STATUS.IN_PROGRESS
-        #     task_result_id.commit()
+            event = dict(request.json)
+        resp = TaskManager(
+            project_id=project_id, mode=self.mode
+        ).run_task(event, task.task_id)
         return resp, resp.get('code', 200)
 
-    @auth.decorators.check_api(["configuration.tasks.tasks.edit"])
-    def put(self, project_id: int, task_id: str):
-        args = request.json
-        project, task = self._get_task(project_id, task_id)
-        task.task_handler = args.get("invoke_func")
-        task.region = args.get("region")
-        task.env_vars = args.get("env_vars")
-        task.commit()
-        return task.to_json(), 200
+    # @auth.decorators.check_api(["configuration.tasks.tasks.edit"])
+    # def put(self, project_id: int, task_id: str):
+    #     # TODO: this is not how you update tasks!
+    #     return None, 404
+    #     task = self._get_task(project_id, task_id)
+    #     # args = request.json
+    #     # task.task_handler = args.get("invoke_func")
+    #     # task.region = args.get("region")
+    #     # task.env_vars = args.get("env_vars")
+    #     # task.commit()
+    #     return task.to_json(), 200
 
     @auth.decorators.check_api(["configuration.tasks.tasks.delete"])
     def delete(self, project_id: int, task_id: str):
-        project, task = self._get_task(project_id, task_id)  # todo: why do we extra query project?
-        task.delete()
+        # TODO: maybe we need to delete artifact?
+        # TODO: for core tasks we need either update or delete id from secrets
+        Task.query.filter(
+            Task.task_id == task_id,
+            Task.mode == self.mode,
+            Task.project_id == project_id
+        ).delete()
         return None, 204
 
 
 class AdminApi(api_tools.APIModeHandler):
     def _get_task(self, task_id: str):
-        return Task.query.filter(Task.task_id == task_id, Task.mode == self.mode).first()
+        return Task.query.filter(
+            Task.task_id == task_id, Task.mode == self.mode
+        ).first_or_404()
 
     @auth.decorators.check_api(["configuration.tasks.tasks.view"])
     def get(self, task_id: str, **kwargs):
@@ -79,32 +82,25 @@ class AdminApi(api_tools.APIModeHandler):
         try:
             event = [{row['name']: row['default'] for row in request.json}]
         except:
-            event = request.json
+            event = dict(request.json)
         resp = TaskManager(mode=self.mode).run_task(event, task.task_id)
-        # todo: why do you think task_result_id will be correct?
-        # task_result_id = TaskResults.query.filter(
-        #     TaskResults.task_id == task_id
-        # ).order_by(
-        #     TaskResults.id.desc()
-        # ).first()
-        # if resp['code'] == 200 and task_result_id:
-        #     task_result_id.task_status = TASK_STATUS.IN_PROGRESS
-        #     task_result_id.commit()
         return resp, resp['code']
 
-    @auth.decorators.check_api(["configuration.tasks.tasks.edit"])
-    def put(self, task_id: str, **kwargs):
-        task = self._get_task(task_id)
-        task.task_handler = request.json.get("invoke_func", task.task_handler)
-        task.region = request.json.get("region", task.region)
-        task.env_vars = request.json.get("env_vars", task.env_vars)
-        task.commit()
-        return task.to_json(), 200
+    # @auth.decorators.check_api(["configuration.tasks.tasks.edit"])
+    # def put(self, task_id: str, **kwargs):
+    #     # TODO: this is not how you update tasks!
+    #     return None, 404
+    #     # task = self._get_task(task_id)
+    #     # task.task_handler = request.json.get("invoke_func", task.task_handler)
+    #     # task.region = request.json.get("region", task.region)
+    #     # task.env_vars = request.json.get("env_vars", task.env_vars)
+    #     # task.commit()
+    #     return task.to_json(), 200
 
     @auth.decorators.check_api(["configuration.tasks.tasks.delete"])
     def delete(self, task_id: str, **kwargs):
-        # task = self._get_task(task_id)
-        # task.delete()
+        # TODO: maybe we need to delete artifact?
+        # TODO: for core tasks we need either update or delete id from secrets
         Task.query.filter(Task.task_id == task_id, Task.mode == self.mode).delete()
         return None, 204
 
